f01d6a57417284a7f83499c6ca197039175d48b4
b00982898 <b00982898@MF116PC37>
2025-09-29 14:44:06 +0100

    Add project files.

---
+++ WEEK1_Practical(2)/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace WEEK1_Practical_2_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
